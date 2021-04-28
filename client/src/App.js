85d1a8f59694ce96b65e2aeee33d820b98ecf8c2
Matt <mattcharlesh@gmail.com>
2021-04-28 12:47:27 -0400

    Update App.js
    basic react router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import {
   ChakraProvider,
   Box,
   Text,
-  Link,
   VStack,
   Code,
   Grid,
   theme,
 } from '@chakra-ui/react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      
-          
+      <Fragment>
+        <Router>
+          <Switch>
+            <Route exact 
+            path="/login" 
+            render={props => <Login {...props} />}
+            />
+            <Route
+              exact
+              path="/register"
+              render={props => <Register {...props} />}
+            />
+            <Route
+              exact
+              path="/dashboard"
+              render={props => <Dashboard {...props} />}
+            />
+          </Switch>
+        </Router>
+      </Fragment>
     </ChakraProvider>
   );
 }
