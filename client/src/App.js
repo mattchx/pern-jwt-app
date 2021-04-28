2e806f9b2c2e85e72c963fa47f81d893483df626
Matt <mattcharlesh@gmail.com>
2021-04-28 13:10:39 -0400

    Update App.js
    private routes added

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import {
   ChakraProvider,
   Box,
@@ -8,31 +8,58 @@ import {
   Grid,
   theme,
 } from '@chakra-ui/react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link,
+} from 'react-router-dom';
 
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import Register from './components/Register';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <ChakraProvider theme={theme}>
       <Fragment>
         <Router>
           <Switch>
-            <Route exact 
-            path="/login" 
-            render={props => <Login {...props} />}
+            <Route
+              exact
+              path="/login"
+              render={props =>
+                !isAuthenticated ? (
+                  <Login {...props} />
+                ) : (
+                  <Redirect to="/dashboard" />
+                )
+              }
             />
             <Route
               exact
               path="/register"
-              render={props => <Register {...props} />}
+              render={props =>
+                !isAuthenticated ? (
+                  <Register {...props} />
+                ) : (
+                  <Redirect to="/login" />
+                )
+              }
             />
             <Route
               exact
               path="/dashboard"
-              render={props => <Dashboard {...props} />}
+              render={props =>
+                !isAuthenticated ? (
+                  <Dashboard {...props} />
+                ) : (
+                  <Redirect to="/login" />
+                )
+              }
             />
           </Switch>
         </Router>
