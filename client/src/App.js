1cdd2aa47cf4baa90fa88e37ed03b9430643a8e8
Matt <mattcharlesh@gmail.com>
2021-04-28 12:26:02 -0400

    added client CRA with Chakra UI

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      
+          
+    </ChakraProvider>
+  );
+}
+
+export default App;
